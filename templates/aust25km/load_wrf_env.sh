85a3410f8e252508ccc4ca55a0165b397c8b9ef0
Peter Rayner <prayner@unimelb.edu.au>
2023-04-19 13:29:04 +1000

    initial nameist templates from Sougol

---
+++ templates/aust25km/load_wrf_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## the following group should be the *same* as used in the WRF build.env files
+## that contains the environment modules used in WRF.
+module load intel-compiler/2019.3.199
+module load openmpi/4.0.2
+module load hdf5/1.10.5
+module load netcdf/4.7.1

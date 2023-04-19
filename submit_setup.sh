49c47e64bf4ffc8c93510a1ed257122d63b1abb7
Peter Rayner <prayner@unimelb.edu.au>
2023-04-19 12:07:21 +1000

    versions from Sougol

--- submit_setup.sh
+++ submit_setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #PBS -N setupWRF
 #PBS -l walltime=10:00:00
-#PBS -l mem=4GB
+#PBS -l mem=16GB
 #PBS -l ncpus=1
 #PBS -j oe
 #PBS -q copyq
 #PBS -l wd
-#PBS -l storage=scratch/${PROJECT}+gdata/sx70+gdata/hh5+gdata/ua8+gdata/ub4
+#PBS -l storage=gdata/sx70+gdata/hh5+gdata/ua8+gdata/ub4
+#PBS -P q90
 
 source load_conda_env.sh
 

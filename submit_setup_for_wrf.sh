f98e4c823863ed64cea9f964f668517554a99231
Peter Rayner <prayner@unimelb.edu.au>
2023-10-31 11:28:03 +1100

    adding submit script

---
+++ submit_setup_for_wrf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#PBS -P q90
+#PBS -q copyq
+#PBS -N setup_for_wrf
+#PBS -l walltime=10:00:00,mem=96GB
+#PBS -l storage=scratch/lp86+gdata/hh5+gdata/lp86
+#PBS -l ncpus=1
+#PBS -l wd
+###PBS -j oe
+
+source /home/563/pjr563/openmethane-beta/setup_wrf/load_conda_env.sh
+
+python3 setup_for_wrf.py

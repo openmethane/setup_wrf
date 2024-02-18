f9f90b89e7d6d6de84f463bfaec4088b270d43cb
Peter Rayner <prayner@unimelb.edu.au>
2024-02-18 12:40:01 +1100

    updating conda version

--- make_grdfile.sh
+++ make_grdfile.sh
@@ -5,7 +5,7 @@
 module purge
 module load pbs dot nco 
 module use /g/data3/hh5/public/modules/ 
-module load conda/gdal27 
+module load conda/analysis3
 export GRIDDESC="/home/563/pjr563/scratch/openmethane-beta/mcip/2022-07-01/d01/GRIDDESC"
 export GRDFILE="/home/563/pjr563/scratch/openmethane-beta/mcip/2022-07-01/d01/GRDFILE.nc"
 export BDYFILE="/home/563/pjr563/scratch/openmethane-beta/mcip/2022-07-01/d01/GRIDBDY2D_1"
